7cd4b19e79897108a25521507f8842894f8f90fc
alexvedi <61770719+alexvedi@users.noreply.github.com>
2025-07-03 21:19:14 -0500

    V1

---
+++ script.js
@@ -0,0 +1,11 @@
+LottieInteractivity.create({
+  player: '#firstLottie',
+  mode: 'cursor',
+  actions: [
+    {
+        position: { x: [0, 1], y: [-1, 2] },
+        type: 'seek',
+        frames: [0, 150],
+    }
+  ]
+});
